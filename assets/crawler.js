1776c4657f8cbc3f9552b86422477ab1909be14f
xavier-delfini <xavier.delfini@laplateforme.io>
2023-06-02 10:21:23 +0200

    Suppression des anciens fichiers

--- assets/crawler.js
+++
@@ -1,8 +0,0 @@
-import puppeteer from "puppeteer";
-
-(async() => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-
-    await page.goto('https://developer.chrome.com/');
-})
